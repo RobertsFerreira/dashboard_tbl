519d44e37ff354be4f8c57607c96325479a7c152
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2022-11-08 23:38:18 -0300

    alterações necessária

--- lib/components/answer_card.dart
+++ lib/components/answer_card.dart
@@ -6,6 +6,7 @@ class AnswerCard extends StatelessWidget {
   final Function(int?)? onChanged;
   final String text;
   final int limitScore;
+  final bool trailing;
   const AnswerCard({
     Key? key,
     required this.index,
@@ -13,10 +14,10 @@ class AnswerCard extends StatelessWidget {
     required this.text,
     required this.limitScore,
     this.onChanged,
+    this.trailing = true,
   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
+  Widget withTrailing() {
     return Center(
       child: Card(
         child: SizedBox(
@@ -72,5 +73,70 @@ class AnswerCard extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget withoutTrailing() {
+    return Center(
+      child: Card(
+        child: SizedBox(
+          height: 80,
+          child: Center(
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                CircleAvatar(
+                  child: Text(
+                    (index + 1).toString(),
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      text,
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(right: 10),
+                  child: Center(
+                    child: DropdownButtonFormField<int>(
+                      value: pointSelect,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      items: List.generate(
+                        limitScore,
+                        (index) {
+                          return DropdownMenuItem<int>(
+                            value: index,
+                            child: Text(
+                              index.toString(),
+                            ),
+                          );
+                        },
+                      ),
+                      onChanged: onChanged,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (trailing) {
+      return withTrailing();
+    }
+    return withoutTrailing();
   }
 }
